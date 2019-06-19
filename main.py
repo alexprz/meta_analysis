d4c4280d6291bb1356a7bcfea2cb67e516986395
Unknown <alexperez@wanadoo.fr>
2019-06-19 17:21:21 +0200

    Added print

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import nibabel as nib
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
+from time import time
 
 
 input_path = 'minimal/'
@@ -69,6 +70,7 @@ if __name__ == '__main__':
 
 
     # Step 2
+    time0 = time()
     corpus_tfidf = scipy.sparse.load_npz(input_path+'corpus_tfidf.npz')
 
     # feature_file = open(input_path+'feature_names.txt')
@@ -107,11 +109,12 @@ if __name__ == '__main__':
 
     # print(nonzero_pmids)
 
-    feature_id = encode_feature['memory']
+    feature_id = encode_feature['amygdala']
     # frequencies = corpus_tfidf[:, feature_id].nonzero()[0]
+    print('Get nonzero pmid')
     nonzero_pmids = np.array([int(decode_pmid[index]) for index in corpus_tfidf[:, feature_id].nonzero()[0]])
-    print(nonzero_pmids)
-    print(nonzero_pmids.shape)
+    # print(nonzero_pmids)
+    # print(nonzero_pmids.shape)
 
     coordinates = pd.read_csv(input_path+'coordinates.csv')
     stat_img_data = np.zeros((Ni,Nj,Nk)) # Building blank stat_img with MNI152's shape
@@ -128,8 +131,12 @@ if __name__ == '__main__':
     #         # i, j, k, _ = np.rint(np.dot(inv_affine, [x, y, z, 1])).astype(int)
     #         # print(i, j, k)
     #         stat_img_data[i, j, k] += 1#corpus_tfidf[encode_pmid[str(pmid)], feature_id]
+    print('Get nonzero coordinates')
     nonzero_coordinates = coordinates.loc[coordinates['pmid'].isin(nonzero_pmids)]
-    frequencies = np.array([corpus_tfidf[encode_pmid[str(pmid)], feature_id] for pmid in nonzero_coordinates['pmid']])
+    print('Get pmid')
+    pmids = np.array(nonzero_coordinates['pmid'])
+    # print('Build frequencies')
+    # frequencies = np.array([corpus_tfidf[encode_pmid[str(pmid)], feature_id] for pmid in nonzero_coordinates['pmid']])
     # print(data)
     print(len(nonzero_coordinates))
     n_sample = len(nonzero_coordinates)
@@ -137,23 +144,29 @@ if __name__ == '__main__':
     # # nonzero_coordinates['feature_frequency'] = np.array(data)
     # print(nonzero_coordinates)
 
+    print('Build coords')
     coord = np.zeros((n_sample, 3))
     coord[:, 0] = nonzero_coordinates['x']
     coord[:, 1] = nonzero_coordinates['y']
     coord[:, 2] = nonzero_coordinates['z']
 
+    print('Build voxel coords')
     voxel_coords = np.zeros((n_sample, 3)).astype(int)
     for k in range(n_sample):
         voxel_coords[k, :] = np.minimum(np.floor(np.dot(inv_affine, [coord[k, 0], coord[k, 1], coord[k, 2], 1]))[:-1].astype(int), [Ni-1, Nj-1, Nk-1])
 
-    print(coord)
-    print(voxel_coords)
-    print(frequencies)
+    # print(coord)
+    # print(voxel_coords)
+    # print(frequencies)
 
     print('Building map')
-    for i, j, k in voxel_coords:
+    for index, value in enumerate(voxel_coords):
         # print(i, j, k)
-        stat_img_data[i, j, k] += 1
+        i, j, k = value
+        # print(encode_pmid[str(pmids[index])])
+        # stat_img_data[i, j, k] += corpus_tfidf[encode_pmid[str(pmids[index])], feature_id]
+        stat_img_data[i, j, k] += corpus_tfidf[encode_pmid[str(pmids[index])], feature_id]
+        # stat_img_data[i, j, k] += frequencies[index]
     # print(len(nonzero_coordinates))
 
     # for index, row in nonzero_coordinates.iterrows():
@@ -170,6 +183,6 @@ if __name__ == '__main__':
     #     stat_img_data[i, j, k] += 1
 
 
-
+    print('Building time : {}'.format(time()-time0))
     stat_img_data = gaussian_filter(stat_img_data, sigma=1)
-    plot_activity_map(nib.Nifti1Image(stat_img_data, affine), glass_brain=True)
+    plot_activity_map(nib.Nifti1Image(stat_img_data, affine), glass_brain=True, threshold=0.3)
