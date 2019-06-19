98e89c8b874544ffc14dbe0d461f21a1b1cd3ec2
Unknown <alexperez@wanadoo.fr>
2019-06-19 17:52:42 +0200

    Cleaned code

--- main.py
+++ main.py
@@ -61,89 +61,29 @@ def build_index(file_name):
     
     return encode, decode
 
-
-if __name__ == '__main__':
-    # Step 1 : Plot activity map from a given pmid
-    # pmid = 22266924 
-    # stat_img = build_activity_map_from_pmid(pmid, sigma=1.5)
-    # plot_activity_map(stat_img, glass_brain=True)
-
-
-    # Step 2
+def build_activity_map_from_keyword(keyword, sigma=1):
     time0 = time()
     corpus_tfidf = scipy.sparse.load_npz(input_path+'corpus_tfidf.npz')
 
-    # feature_file = open(input_path+'feature_names.txt')
-    # feature_index = dict()
-    # k = 0
-    # for line in feature_file:
-    #     # print(line.strip())
-    #     feature_index[line.strip()] = k
-    #     k += 1
-
-    # pmids_file = open(input_path+'pmids.txt')
-    # pmids_index = dict()
-    # k = 0
-    # for line in pmids_file:
-    #     # print(line.strip())
-    #     pmids_index[int(line.strip())] = k
-    #     k += 1
-    
-    # print(feature_index)
-    # print(pmids_index)
-    # print(corpus_tfidf[13880, 6179])
-    # print(corpus_tfidf[pmids_index[15522765], feature_index['a1']])
-
-    # def build_activity_map_from_feature(feature_name, sigma=1):
-    #     feature_id = feature_index[feature_name]
-    #     print(feature_id)
-    #     frequencies = corpus_tfidf[corpus_tfidf[:, feature_id].nonzero()[0], feature_id]
-    #     print(frequencies)
-
-    # build_activity_map_from_feature('memory')
-
     encode_feature, decode_feature = build_index('feature_names.txt')
     encode_pmid, decode_pmid = build_index('pmids.txt')
+    
+    feature_id = encode_feature[keyword]
 
-    # nonzero_pmids = np.array([pmid[0, 0] for pmid in corpus_tfidf[:, encode_feature['cognitive']]])
-
-    # print(nonzero_pmids)
-
-    feature_id = encode_feature['amygdala']
-    # frequencies = corpus_tfidf[:, feature_id].nonzero()[0]
     print('Get nonzero pmid')
     nonzero_pmids = np.array([int(decode_pmid[index]) for index in corpus_tfidf[:, feature_id].nonzero()[0]])
-    # print(nonzero_pmids)
-    # print(nonzero_pmids.shape)
 
     coordinates = pd.read_csv(input_path+'coordinates.csv')
     stat_img_data = np.zeros((Ni,Nj,Nk)) # Building blank stat_img with MNI152's shape
     
-    # nb = len(nonzero_pmids)
-    # count = 0
-    # for pmid in nonzero_pmids:
-    #     print('{} out of {}'.format(count, nb))
-    #     count += 1
-    #     for index, row in coordinates.loc[coordinates['pmid'] == pmid].iterrows():
-    #         x, y, z = row['x'], row['y'], row['z']
-    #         i, j, k, _ = np.floor(np.dot(inv_affine, [x, y, z, 1])).astype(int)
-    #         i, j, k = np.minimum([i, j, k], [Ni-1, Nj-1, Nk-1])
-    #         # i, j, k, _ = np.rint(np.dot(inv_affine, [x, y, z, 1])).astype(int)
-    #         # print(i, j, k)
-    #         stat_img_data[i, j, k] += 1#corpus_tfidf[encode_pmid[str(pmid)], feature_id]
     print('Get nonzero coordinates')
     nonzero_coordinates = coordinates.loc[coordinates['pmid'].isin(nonzero_pmids)]
     print('Get pmid')
     pmids = np.array(nonzero_coordinates['pmid'])
-    # print('Build frequencies')
-    # frequencies = np.array([corpus_tfidf[encode_pmid[str(pmid)], feature_id] for pmid in nonzero_coordinates['pmid']])
-    # print(data)
+    print('Build frequencies')
+    frequencies = np.array([corpus_tfidf[encode_pmid[str(pmid)], feature_id] for pmid in nonzero_coordinates['pmid']])
     print(len(nonzero_coordinates))
     n_sample = len(nonzero_coordinates)
-    # nonzero_coordinates['feature_frequency'] = pd.Series(data, index=nonzero_coordinates.index)
-    # # nonzero_coordinates['feature_frequency'] = np.array(data)
-    # print(nonzero_coordinates)
-
     print('Build coords')
     coord = np.zeros((n_sample, 3))
     coord[:, 0] = nonzero_coordinates['x']
@@ -155,34 +95,24 @@ if __name__ == '__main__':
     for k in range(n_sample):
         voxel_coords[k, :] = np.minimum(np.floor(np.dot(inv_affine, [coord[k, 0], coord[k, 1], coord[k, 2], 1]))[:-1].astype(int), [Ni-1, Nj-1, Nk-1])
 
-    # print(coord)
-    # print(voxel_coords)
-    # print(frequencies)
-
     print('Building map')
     for index, value in enumerate(voxel_coords):
-        # print(i, j, k)
         i, j, k = value
-        # print(encode_pmid[str(pmids[index])])
         # stat_img_data[i, j, k] += corpus_tfidf[encode_pmid[str(pmids[index])], feature_id]
-        stat_img_data[i, j, k] += corpus_tfidf[encode_pmid[str(pmids[index])], feature_id]
-        # stat_img_data[i, j, k] += frequencies[index]
-    # print(len(nonzero_coordinates))
-
-    # for index, row in nonzero_coordinates.iterrows():
-    #     pmid = row['pmid']
-    #     print(index)
-    #     # if corpus_tfidf[encode_pmid[str(pmid)], feature_id] == 0:
-    #     #     # print('continue')
-    #     #     continue
-    #     x, y, z = row['x'], row['y'], row['z']
-    #     i, j, k, _ = np.floor(np.dot(inv_affine, [x, y, z, 1])).astype(int)
-    #     i, j, k = np.minimum([i, j, k], [Ni-1, Nj-1, Nk-1])
-    #     # i, j, k, _ = np.rint(np.dot(inv_affine, [x, y, z, 1])).astype(int)
-    #     # print(i, j, k)
-    #     stat_img_data[i, j, k] += 1
-
+        stat_img_data[i, j, k] += frequencies[index]
 
     print('Building time : {}'.format(time()-time0))
     stat_img_data = gaussian_filter(stat_img_data, sigma=1)
-    plot_activity_map(nib.Nifti1Image(stat_img_data, affine), glass_brain=True, threshold=0.3)
+    return nib.Nifti1Image(stat_img_data, affine)
+
+if __name__ == '__main__':
+    # Step 1 : Plot activity map from a given pmid
+    # pmid = 22266924 
+    # stat_img = build_activity_map_from_pmid(pmid, sigma=1.5)
+    # plot_activity_map(stat_img, glass_brain=True)
+
+
+    # Step 2
+    keyword = 'amygdala'
+    stat_img = build_activity_map_from_keyword(keyword, sigma=1.5)
+    plot_activity_map(stat_img, glass_brain=True, threshold=0.3)
