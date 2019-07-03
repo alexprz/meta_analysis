b9d976f2351c35efe94ad3c796e304aaf52db9a3
Unknown <alexperez@wanadoo.fr>
2019-07-03 12:16:44 -0400

    Added reduce feature to change resolution of covariance matrix

--- covariance.py
+++ covariance.py
@@ -1,11 +1,12 @@
-from globals import mem, coordinates, corpus_tfidf, Ni, Nj, Nk, inv_affine
-from builds import encode_pmid, encode_feature, decode_pmid, decode_feature
 from time import time
 import numpy as np
 import scipy
 
+from globals import mem, coordinates, corpus_tfidf, Ni, Nj, Nk, affine, inv_affine
+from builds import encode_pmid, encode_feature, decode_pmid, decode_feature
+
 @mem.cache
-def build_covariance_matrix_from_keyword(keyword, sigma=2.):
+def build_covariance_matrix_from_keyword(keyword, gaussian_filter=False, sigma=2., reduce=2):
     '''
         Build empirical covariance matrix of the voxel of the activity map associated to the given keyword
     '''
@@ -16,10 +17,19 @@ def build_covariance_matrix_from_keyword(keyword, sigma=2.):
     print('Get nonzero pmid')
     nonzero_pmids = np.array([int(decode_pmid[index]) for index in corpus_tfidf[:, feature_id].nonzero()[0]])
 
-    stat_img_data = np.zeros((Ni,Nj,Nk)) # Building blank stat_img with MNI152's shape
+    # Change box size according to reduce factor
+    Ni_r, Nj_r, Nk_r = np.ceil(np.array((Ni, Nj, Nk))/reduce).astype(int)
 
+    stat_img_data = np.zeros((Ni_r,Nj_r,Nk_r)) # Building blank stat_img with MNI152's shape
     n_observations = len(nonzero_pmids)
-    observations = np.zeros((n_observations, Ni*Nj*Nk))
+    observations = np.zeros((n_observations, Ni_r*Nj_r*Nk_r))
+
+    # Change affine to new box size
+    affine_r = np.copy(affine)
+    for i in range(3):
+        affine_r[i, i] = affine[i, i]*reduce
+
+    inv_affine_r = np.linalg.inv(affine_r)
 
     for i, pmid in enumerate(nonzero_pmids):
         # For each coordinates found in pmid (in mm), compute its corresponding voxels coordinates
@@ -27,15 +37,16 @@ def build_covariance_matrix_from_keyword(keyword, sigma=2.):
         print('{} out of {}'.format(i, n_observations))
         for index, row in coordinates.loc[coordinates['pmid'] == pmid].iterrows():
             x, y, z = row['x'], row['y'], row['z']
-            i, j, k = np.minimum(np.floor(np.dot(inv_affine, [x, y, z, 1]))[:-1].astype(int), [Ni-1, Nj-1, Nk-1])
+            i, j, k = np.minimum(np.floor(np.dot(inv_affine_r, [x, y, z, 1]))[:-1].astype(int), [Ni_r-1, Nj_r-1, Nk_r-1])
             stat_img_data[i, j, k] += 1
         
-        # With gaussian blurr, sparse calculation isn't efficient (not enough zeros)
-        # stat_img_data = gaussian_filter(stat_img_data, sigma=sigma, truncate=1.)
+        # With gaussian blurr, sparse calculation may not be efficient (not enough zeros)
+        if gaussian_filter:
+            stat_img_data = scipy.ndimage.gaussian_filter(stat_img_data, sigma=sigma)
 
         observations[i, :] = stat_img_data.flatten()
 
-
+    # Sparse computation of covariance matrix
     s_X = scipy.sparse.csr_matrix(observations)
     s_Ones = scipy.sparse.csr_matrix(np.ones(n_observations))
 
@@ -45,7 +56,13 @@ def build_covariance_matrix_from_keyword(keyword, sigma=2.):
 
     s_cov_matrix = M1/n_observations - M2.dot(M3)/(n_observations**2)
 
-    return s_cov_matrix
+    return s_cov_matrix, affine_r
+
 
 if __name__ == '__main__':
-    print(build_covariance_matrix_from_keyword('prosopagnosia'))
+    keyword = 'prosopagnosia'
+    sigma = 2.
+
+    cov_matrix, affine_r = build_covariance_matrix_from_keyword(keyword, sigma=sigma, reduce=10)
+    print(cov_matrix)
+    print(cov_matrix.shape)
