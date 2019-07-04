af0f3756daf690dd8e2ba2aa8f505a9ca402ada1
Unknown <alexperez@wanadoo.fr>
2019-07-04 11:37:22 -0400

    Cleaned code

--- covariance.py
+++ covariance.py
@@ -21,20 +21,17 @@ def compute_map(pmid_enumerate, n_voxels, Ni_r, Nj_r, Nk_r, inv_affine_r, gaussi
 
         # For each coordinates found in pmid (in mm), compute its corresponding voxels coordinates
         # and note it as activated
-        # print('{} out of {}'.format(obs_index, n_observations))
         print_percent(p, n_tot, prefix='Covariance matrix ')
         for index, row in coordinates.loc[coordinates['pmid'] == pmid].iterrows():
             x, y, z = row['x'], row['y'], row['z']
             i, j, k = np.minimum(np.floor(np.dot(inv_affine_r, [x, y, z, 1]))[:-1].astype(int), [Ni_r-1, Nj_r-1, Nk_r-1])
             stat_img_data[i, j, k] += 1
         
-        # With gaussian blurr, sparse calculation may not be efficient (not enough zeros)
+        # With gaussian kernel, sparse calculation may not be efficient (not enough zeros)
         if gaussian_filter:
             stat_img_data = scipy.ndimage.gaussian_filter(stat_img_data, sigma=sigma)
 
-        reshaped = stat_img_data.reshape(-1)
-        observations_[p, :] = reshaped
-        # observations[obs_index, :] = reshaped
+        observations_[p, :] = stat_img_data.reshape(-1)
         p += 1
 
     return observations_
@@ -44,11 +41,7 @@ def build_covariance_matrix_from_keyword(keyword, gaussian_filter=False, sigma=2
     '''
         Build empirical covariance matrix of the voxel of the activity map associated to the given keyword
     '''
-    time0 = time()
-
     feature_id = encode_feature[keyword]
-
-    print('Get nonzero pmid')
     nonzero_pmids = np.array([int(decode_pmid[index]) for index in corpus_tfidf[:, feature_id].nonzero()[0]])
 
     # Change box size according to reduce factor
@@ -67,13 +60,7 @@ def build_covariance_matrix_from_keyword(keyword, gaussian_filter=False, sigma=2
     for k in range(Nk_r):
          coords[:, :, k, 2] = k
 
-    # print(coords)
-
     coords = coords.reshape(-1, 3)
-
-    # print(coords)
-
-    observations = np.zeros((n_observations, n_voxels))
 
     # Change affine to new box size
     affine_r = np.copy(affine)
@@ -82,56 +69,9 @@ def build_covariance_matrix_from_keyword(keyword, gaussian_filter=False, sigma=2
 
     inv_affine_r = np.linalg.inv(affine_r)
 
-    # def compute_map(pmid_enumerate):
-    #     n_tot = len(pmid_enumerate)
-    #     observations_ = np.zeros((n_tot, n_voxels))
-    #     p = 0
-    #     for obs_index, pmid in pmid_enumerate:
-    #         stat_img_data = np.zeros((Ni_r,Nj_r,Nk_r)) # Building blank stat_img with MNI152's shape
-
-    #         # For each coordinates found in pmid (in mm), compute its corresponding voxels coordinates
-    #         # and note it as activated
-    #         # print('{} out of {}'.format(obs_index, n_observations))
-    #         print_percent(p, n_tot, prefix='Covariance matrix ')
-    #         for index, row in coordinates.loc[coordinates['pmid'] == pmid].iterrows():
-    #             x, y, z = row['x'], row['y'], row['z']
-    #             i, j, k = np.minimum(np.floor(np.dot(inv_affine_r, [x, y, z, 1]))[:-1].astype(int), [Ni_r-1, Nj_r-1, Nk_r-1])
-    #             stat_img_data[i, j, k] += 1
-            
-    #         # With gaussian blurr, sparse calculation may not be efficient (not enough zeros)
-    #         if gaussian_filter:
-    #             stat_img_data = scipy.ndimage.gaussian_filter(stat_img_data, sigma=sigma)
-
-    #         reshaped = stat_img_data.reshape(-1)
-    #         observations_[p, :] = reshaped
-    #         # observations[obs_index, :] = reshaped
-    #         p += 1
-
-    #     return observations_
-
-        # return stat_img_data.reshape(-1)s
-    time0 = time()
     n_jobs = multiprocessing.cpu_count()-1
-
-    # observations = compute_map(list(enumerate(nonzero_pmids)))
-    # compute_map(list(enumerate(nonzero_pmids)))
-
     splitted_array = np.array_split(np.array(list(enumerate(nonzero_pmids))), n_jobs)
-    # print(np.array_split(np.array(list(enumerate(nonzero_pmids))), n_jobs))
-    # print(splitted_array)
-    results = []
-    # for sub_array in splitted_array:
-    #     results.append(compute_map(sub_array))
-        # for i, pmid in sub_array:
-        #     compute_map(i, pmid)
-    # observations = np.concatenate(results, axis=0)
-
-    # Parallel(n_jobs=8, require='sharedmem')(compute_map(i, pmid) for i, pmid in enumerate(nonzero_pmids))
     observations = np.concatenate(Parallel(n_jobs=n_jobs, backend='multiprocessing')(delayed(compute_map)(sub_array, n_voxels, Ni_r, Nj_r, Nk_r, inv_affine_r, gaussian_filter) for sub_array in splitted_array), axis=0)
-
-    print(observations)
-    print('Computation time : {}s'.format(time()-time0))
-    # print(observations.shape)
 
 
     # Sparse computation of covariance matrix
