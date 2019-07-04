593e055157cf93e98a5c5324c878f3432e81c7cf
Unknown <alexperez@wanadoo.fr>
2019-07-04 17:29:21 -0400

    Added 3D to 1D and 1D to 3D index operation

--- tools.py
+++ tools.py
@@ -2,6 +2,8 @@ from globals import mem
 import scipy
 import numpy as np
 
+from globals import Ni, Nj, Nk
+
 def print_percent(index, total, prefix='', rate=10000):
     if (total//rate) == 0 or index % (total//rate) == 0:
         print(prefix+str(round(100*index/total, 1))+'%')
@@ -36,3 +38,26 @@ def empirical_cov_matrix(observations):
     M3 = s_Ones.dot(s_X)
 
     return M1/n_observations - M2.dot(M3)/(n_observations**2)
+
+def index_3D_to_1D(i, j, k, Ni, Nj, Nk):
+    #...
+    return i + Ni*j + Ni*Nj*k
+
+def index_1D_to_3D(p, Ni, Nj, Nk):
+    k = p//(Ni*Nj)
+    p = p%(Ni*Nj)
+    j = p//Ni
+    i = p%Ni
+    return i, j, k
+
+if __name__ == '__main__':
+    i, j, k = 3, 4, 5
+
+    p = index_3D_to_1D(i, j, k, Ni, Nj, Nk)
+    print(p)
+    print(index_1D_to_3D(p, Ni, Nj, Nk))
+
+    p = 34789
+    i, j, k = index_1D_to_3D(p, Ni, Nj, Nk)
+    print(i, j, k)
+    print(index_3D_to_1D(i, j, k, Ni, Nj, Nk))
