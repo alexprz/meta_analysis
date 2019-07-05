159377c388346ae8bfc3e816f0eb8aef246a7965
Unknown <alexperez@wanadoo.fr>
2019-07-05 15:20:19 -0400

    Added docstrings

--- tools.py
+++ tools.py
@@ -39,9 +39,17 @@ def empirical_cov_matrix(observations):
     return M1/n_observations - M2.dot(M3)/(n_observations**2)
 
 def index_3D_to_1D(i, j, k, Ni, Nj, Nk):
+    '''
+        Changes indexing from 3D to 1D Fortran like (first index moving fastest).
+        Doesn't check bounds.
+    '''
     return i + Ni*j + Ni*Nj*k
 
 def index_1D_to_3D(p, Ni, Nj, Nk):
+    '''
+        Changes indexing from 1D to 3D Fortran like (first index moving fastest).
+        Doesn't check bounds.
+    '''
     k = p//(Ni*Nj)
     p = p%(Ni*Nj)
     j = p//Ni
@@ -49,6 +57,9 @@ def index_1D_to_3D(p, Ni, Nj, Nk):
     return i, j, k
 
 def index_3D_to_1D_checked(i, j, k, Ni, Nj, Nk):
+    '''
+        Equivalent to index_3D_to_1D but checks bounds.
+    '''
     if i >= Ni or j >= Nj or k >= Nk or i < 0 or j < 0 or k < 0:
         raise ValueError('Indices ({}, {}, {}) are outside box of size ({}, {}, {}).'.format(i, j, k, Ni, Nj, Nk))
 
@@ -58,6 +69,9 @@ def index_3D_to_1D_checked(i, j, k, Ni, Nj, Nk):
     return index_3D_to_1D(i, j, k, Ni, Nj, Nk)
 
 def index_1D_to_3D_checked(p, Ni, Nj, Nk):
+    '''
+        Equivalent to index_1D_to_3D but checks bounds.
+    '''
     size = Ni*Nj*Nk
     if p >= size or p < 0:
         raise ValueError('Indice {} is outside vector of size {}*{}*{}={}.'.format(p, Ni, Nj, Nk, size))
@@ -71,7 +85,7 @@ def map_to_data(map, Ni, Nj, Nk):
     '''
         Convert a sparse CSR matrix of shape (n_voxels, 1) into a dense 3D numpy array of shape (Ni, Nj, Nk).
 
-        Indexing of map is supposed to have been made Fortran like.
+        Indexing of map is supposed to have been made Fortran like (first index moving fastest).
     '''
     n_voxels, _ = map.shape
 
