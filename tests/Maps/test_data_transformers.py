5f3170dbcae2113f1a53ca8368f2a7e5aeac3c10
PEREZ <alexperez@wanadoo.fr>
2019-07-25 13:47:41 -0400

    added to_img test case

--- tests/Maps/test_data_transformers.py
+++ tests/Maps/test_data_transformers.py
@@ -42,3 +42,55 @@ class ToArrayTestCase(unittest.TestCase):
         maps = Maps(self.array4D_2)
         self.assertTrue(np.array_equal(maps.to_array(0), self.array4D_2[:, :, :, 0]))
         
+class ToImgTestCase(unittest.TestCase):
+    def setUp(self):
+        self.array2D = np.random.rand(2, 2)
+        self.array4D = np.random.rand(3, 3, 3, 1)
+        self.array4D_2 = np.random.rand(3, 3, 3, 2)
+        self.array3D = np.random.rand(3, 3, 3)
+        self.affine = np.eye(4)
+
+    def test_array_2D(self):
+        maps = Maps(self.array2D, Ni=2, Nj=1, Nk=1, affine=self.affine)
+        img = maps.to_img()
+        self.assertTrue(np.array_equal(img.get_fdata(), self.array2D.reshape((2, 1, 1, 2))))
+        self.assertTrue(np.array_equal(img.affine, self.affine))
+    
+    def test_array_2D_one(self):
+        maps = Maps(self.array2D, Ni=2, Nj=1, Nk=1, affine=self.affine)
+        img0 = maps.to_img(0)
+        img1 = maps.to_img(1)
+        self.assertTrue(np.array_equal(img0.get_fdata(), self.array2D.reshape((2, 1, 1, 2))[:, :, :, 0]))
+        self.assertTrue(np.array_equal(img1.get_fdata(), self.array2D.reshape((2, 1, 1, 2))[:, :, :, 1]))
+        self.assertTrue(np.array_equal(img0.affine, self.affine))    
+        self.assertTrue(np.array_equal(img1.affine, self.affine))
+
+    def test_array_3D(self):
+        maps = Maps(self.array3D, affine=self.affine)
+        img = maps.to_img()
+        self.assertTrue(np.array_equal(img.get_fdata(), self.array3D))
+        self.assertTrue(np.array_equal(img.affine, self.affine))
+
+    def test_array_4D(self):
+        maps = Maps(self.array4D, affine=self.affine)
+        img = maps.to_img()
+        self.assertTrue(np.array_equal(img.get_fdata(), self.array4D[:, :, :, 0]))
+        self.assertTrue(np.array_equal(img.affine, self.affine))
+
+    def test_array_4D_one(self):
+        maps = Maps(self.array4D, affine=self.affine)
+        img = maps.to_img(0)
+        self.assertTrue(np.array_equal(img.get_fdata(), self.array4D[:, :, :, 0]))
+        self.assertTrue(np.array_equal(img.affine, self.affine))
+
+    def test_array_4D_2_all(self):
+        maps = Maps(self.array4D_2, affine=self.affine)
+        img = maps.to_img()
+        self.assertTrue(np.array_equal(img.get_fdata(), self.array4D_2))
+        self.assertTrue(np.array_equal(img.affine, self.affine))
+
+    def test_array_4D_2_one(self):
+        maps = Maps(self.array4D_2, affine=self.affine)
+        img = maps.to_img(0)
+        self.assertTrue(np.array_equal(img.get_fdata(), self.array4D_2[:, :, :, 0]))
+        self.assertTrue(np.array_equal(img.affine, self.affine))
