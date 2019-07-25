844831b5efb247006579d721df9fb3add4266d68
PEREZ <alexperez@wanadoo.fr>
2019-07-25 13:52:56 -0400

    Changed tests due to change in expected 4D array shape

--- tests/Maps/test_public_tools.py
+++ tests/Maps/test_public_tools.py
@@ -11,12 +11,9 @@ class ApplyMaskTestCase(unittest.TestCase):
                                 [4, 5, 6],
                                 [7, 8, 9]]])
 
-        self.array2 = np.array([[[[1, 2, 3],
-                                 [4, 5, 6],
-                                 [7, 8, 9]]],
-                                 [[[9, 8, 7],
-                                 [6, 5, 4],
-                                 [3, 2, 1]]]])
+        self.array2 = np.array([[[[1, 9], [2, 8], [3, 7]],
+                                 [[4, 6], [5, 5], [6, 4]],
+                                 [[7, 3], [8, 2], [9, 1]]]])
 
         self.mask_data = np.array([[[0, 0, 1],
                                     [0, 1, 0],
@@ -28,12 +25,9 @@ class ApplyMaskTestCase(unittest.TestCase):
                                    [0, 5, 0],
                                    [0, 0, 0]]])
 
-        self.expected2 = np.array([[[[0, 0, 3],
-                                   [0, 5, 0],
-                                   [0, 0, 0]]],
-                                   [[[0, 0, 7],
-                                   [0, 5, 0],
-                                   [0, 0, 0]]]])
+        self.expected2 = np.array([[[[0, 0], [0, 0], [3, 7]],
+                                 [[0, 0], [5, 5], [0, 0]],
+                                 [[0, 0], [0, 0], [0, 0]]]])
         
         self.Ni, self.Nj, self.Nk = self.mask_data.shape
 
@@ -43,8 +37,8 @@ class ApplyMaskTestCase(unittest.TestCase):
 
     def test_mask_init_two_maps(self):
         maps2 = Maps(self.array2, Ni=self.Ni, Nj=self.Nj, Nk=self.Nk, mask=self.mask)
-        self.assertTrue(np.array_equal(maps2.to_array(0), self.expected2[0]))
-        self.assertTrue(np.array_equal(maps2.to_array(1), self.expected2[1]))
+        self.assertTrue(np.array_equal(maps2.to_array(0), self.expected2[:, :, :, 0]))
+        self.assertTrue(np.array_equal(maps2.to_array(1), self.expected2[:, :, :, 1]))
     
     def test_mask_apply_one_map(self):
         maps = Maps(self.array, Ni=self.Ni, Nj=self.Nj, Nk=self.Nk)
@@ -56,8 +50,8 @@ class ApplyMaskTestCase(unittest.TestCase):
         maps2 = Maps(self.array2, Ni=self.Ni, Nj=self.Nj, Nk=self.Nk)
         maps2.apply_mask(self.mask)
 
-        self.assertTrue(np.array_equal(maps2.to_array(0), self.expected2[0]))
-        self.assertTrue(np.array_equal(maps2.to_array(1), self.expected2[1]))
+        self.assertTrue(np.array_equal(maps2.to_array(0), self.expected2[:, :, :, 0]))
+        self.assertTrue(np.array_equal(maps2.to_array(1), self.expected2[:, :, :, 1]))
 
 class NormalizeTestCase(unittest.TestCase):
     def setUp(self):
@@ -65,23 +59,17 @@ class NormalizeTestCase(unittest.TestCase):
                                 [0, 1, 0],
                                 [0, 0, 2]]])
 
-        self.array2 = np.array([[[[1, 0, 0],
-                                 [0, 1, 0],
-                                 [0, 0, 2]]],
-                                 [[[0, 0, 0],
-                                 [0, 1, 0],
-                                 [0, 0, 0]]]])
+        self.array2 = np.array([[[[1, 0], [0, 0], [0, 0]],
+                                 [[0, 0], [1, 1], [0, 0]],
+                                 [[0, 0], [0, 0], [2, 0]]]])
         
         self.expected = np.array([[[0.25, 0, 0],
                                    [0, 0.25, 0],
                                    [0, 0, 0.5]]])
 
-        self.expected2 = np.array([[[[0.25, 0, 0],
-                                   [0, 0.25, 0],
-                                   [0, 0, 0.5]]],
-                                   [[[0, 0, 0],
-                                   [0, 1, 0],
-                                   [0, 0, 0]]]])
+        self.expected2 = np.array([[[[0.25, 0], [0, 0], [0, 0]],
+                                   [[0, 0], [0.25, 1], [0, 0]],
+                                   [[0, 0], [0, 0], [0.5, 0]]]])
 
         self.Ni, self.Nj, self.Nk = self.array.shape
 
@@ -98,10 +86,10 @@ class NormalizeTestCase(unittest.TestCase):
         maps2_ = maps2.normalize(inplace=False)
         maps2.normalize(inplace=True)
 
-        self.assertTrue(np.array_equal(maps2.to_array(0), self.expected2[0]))
-        self.assertTrue(np.array_equal(maps2.to_array(1), self.expected2[1]))
-        self.assertTrue(np.array_equal(maps2_.to_array(0), self.expected2[0]))
-        self.assertTrue(np.array_equal(maps2_.to_array(1), self.expected2[1]))
+        self.assertTrue(np.array_equal(maps2.to_array(0), self.expected2[:, :, :, 0]))
+        self.assertTrue(np.array_equal(maps2.to_array(1), self.expected2[:, :, :, 1]))
+        self.assertTrue(np.array_equal(maps2_.to_array(0), self.expected2[:, :, :, 0]))
+        self.assertTrue(np.array_equal(maps2_.to_array(1), self.expected2[:, :, :, 1]))
 
 class RandomizeTestCase(unittest.TestCase):
     def setUp(self):
