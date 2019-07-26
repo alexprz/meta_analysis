2841c01d3583cf9e06600bdde930068be9e80ef1
PEREZ <alexperez@wanadoo.fr>
2019-07-26 14:29:15 -0400

    Added visual tests for dense init

--- tests/Maps/test_visual.py
+++ tests/Maps/test_visual.py
@@ -14,8 +14,10 @@ sigma = 2.
 # Maps
 df = build_df_from_keyword(keyword)
 maps = Maps(df, template=template, groupby_col='pmid')
+maps_dense = Maps(df, template=template, groupby_col='pmid', save_memory=True)
 maps_atlas = Maps(df, template=template, groupby_col='pmid', atlas=atlas)
 avg, var = maps.iterative_smooth_avg_var(compute_var=True, sigma=sigma, bias=False)
+avg_dense, var_dense = maps_dense.iterative_smooth_avg_var(compute_var=True, sigma=sigma, bias=False)
 avg_atlas, var_atlas = maps_atlas.iterative_smooth_avg_var(compute_var=True, sigma=sigma, bias=False)
 
 
@@ -54,6 +56,22 @@ def test_iterative_avg_var_thresholded_1():
 @pytest.mark.mpl_image_compare
 def test_iterative_avg_var_thresholded_2():
     return plotting.plot_activity_map(var.to_img(), threshold=0.00002)
+
+@pytest.mark.mpl_image_compare
+def test_iterative_avg_var_1_dense():
+    return plotting.plot_activity_map(avg_dense.to_img())
+
+@pytest.mark.mpl_image_compare
+def test_iterative_avg_var_2_dense():
+    return plotting.plot_activity_map(var_dense.to_img())
+
+@pytest.mark.mpl_image_compare
+def test_iterative_avg_var_thresholded_1_dense():
+    return plotting.plot_activity_map(avg_dense.to_img(), threshold=0.0007)
+
+@pytest.mark.mpl_image_compare
+def test_iterative_avg_var_thresholded_2_dense():
+    return plotting.plot_activity_map(var_dense.to_img(), threshold=0.00002)
 
 @pytest.mark.mpl_image_compare
 def test_atlas_sum():
