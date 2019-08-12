0ae3283eafb17f8821c3663f3b41b49236e89d9e
PEREZ <alexperez@wanadoo.fr>
2019-08-12 10:48:56 -0400

    PEP8 Conventions

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,8 @@
 from nilearn import datasets
-import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from nilearn.decomposition import DictLearning, CanICA
 
 
 from meta_analysis import Maps, plotting, print_percent
@@ -13,33 +11,38 @@ from tools import build_df_from_keyword
 from globals import template, gray_mask
 from clustering import fit_CanICA, fit_DictLearning, fit_Kmeans, fit_Wards, fit_GroupICA, fit_Model
 
-#_________ATLASES_________#
+# _________ATLASES_________ #
 atlas_HO_0 = datasets.fetch_atlas_harvard_oxford('sub-maxprob-thr0-2mm')
 atlas_HO_25 = datasets.fetch_atlas_harvard_oxford('sub-maxprob-thr25-2mm')
 atlas_HO_50 = datasets.fetch_atlas_harvard_oxford('sub-maxprob-thr50-2mm')
 
 n_labels = len(atlas_HO_0['labels'])
 
-#_________CRITERIA_________#
+
+# _________CRITERIA_________ #
 def pearson_distance(array_ref, array_obs, **kwargs):
     array_ref /= np.sum(array_ref)
     array_obs /= np.sum(array_obs)
-    
+
     with np.errstate(divide='ignore', invalid='ignore'):
         return np.sum(np.nan_to_num(np.true_divide(np.power(array_obs - array_ref, 2), array_ref), posinf=0))
 
+
 def RMS(array_ref, array_obs, **kwargs):
     return np.sqrt(np.mean(np.power(array_ref-array_obs, 2)))
+
 
 def Mahalanobis(array_ref, array_obs, **kwargs):
     x = array_obs - array_ref
     inv_S = np.real(np.linalg.inv(kwargs['S']))
     return np.sqrt(np.dot(x.T, np.dot(inv_S, x)))
 
+
 def zero(arr, arr2, **kwargs):
     return 0
 
-#_________GOODNESS_OF_FIT_________#
+
+# _________GOODNESS_OF_FIT_________ #
 def goodness_of_fit_map(img_ref, img_obs, criterion, **kwargs):
     array_ref, array_obs = img_ref.get_fdata(), img_obs.get_fdata()
 
@@ -48,12 +51,14 @@ def goodness_of_fit_map(img_ref, img_obs, criterion, **kwargs):
 
     return criterion(array_ref, array_obs, **kwargs)
 
+
 def benchmark_atlas(maps, atlas, criterion, verbose=False, **kwargs):
     maps.apply_atlas(atlas, inplace=True)
     return goodness_of_fit_map(maps.to_img(), maps.to_img_atlas(ignore_bg=True), criterion, **kwargs)
 
+
 def benchmark(maps, atlas_dict, criteria, verbose=False, **kwargs):
-    
+
     df_list = []
     k, n_tot = 0, len(atlas_dict)*len(criteria)
 
@@ -67,18 +72,14 @@ def benchmark(maps, atlas_dict, criteria, verbose=False, **kwargs):
     return pd.DataFrame(df_list, columns=['Criterion', 'Atlas', 'Value'])
 
 
-
 if __name__ == '__main__':
-    keyword = 'prosopagnosia'
+    keyword = 'language'
     sigma = 2.
 
-    df = build_df_from_keyword(keyword)
-    maps = Maps(df, template=template, groupby_col='pmid', mask=gray_mask, verbose=True)
-    maps.smooth(sigma, inplace=True, verbose=True)
-    
-    #Build custom atlases
+    train_proportion = 0.5
+    random_state = 0
     n_components = n_labels-1
-    tag = '{}-sigma-{}-{}-components'.format(keyword, sigma, n_components)
+    tag = '{}-sigma-{}-{}-components-RS-{}'.format(keyword, sigma, n_components, random_state)
     load = True
 
     params_CanICA = {
@@ -97,17 +98,28 @@ if __name__ == '__main__':
         'n_parcels': n_components
     }
 
-    imgs = maps.to_img(sequence=True, verbose=True)
-    
-    CanICA_imgs = fit_Model(fit_CanICA, imgs, params_CanICA, tag=tag, load=load).components_img_
-    # DictLearning_imgs = fit_Model(fit_DictLearning, maps.to_img(), params_DictLearning, tag=tag, load=load).components_img_
-    Ward_imgs = fit_Model(fit_Wards, maps.to_img(), params_Wards, tag=tag, load=load).labels_img_
-    Kmeans_imgs = fit_Model(fit_Kmeans, maps.to_img(), params_Kmeans, tag=tag, load=load).labels_img_
+    df = build_df_from_keyword(keyword)
+    maps = Maps(df, template=template, groupby_col='pmid', mask=gray_mask, verbose=True)
+    maps.smooth(sigma, inplace=True, verbose=True)
+    maps_train, maps_test = maps.split(prop=train_proportion, random_state=random_state)
 
-    atlas_CanICA = Maps(CanICA_imgs, template=template).to_atlas(verbose=True)
-    # atlas_DictLearning = Maps(DictLearning_imgs, template=template).to_atlas(verbose=True)
-    atlas_Ward = Maps(Ward_imgs, template=template).to_atlas(verbose=True)
-    atlas_Kmeans = Maps(Kmeans_imgs, template=template).to_atlas(verbose=True)
+    maps_avg = maps_test.avg()
+
+    # imgs_3D_list = maps_train.to_img(sequence=True, verbose=True)
+    imgs_4D = maps_train.to_img()
+
+    CanICA_imgs = fit_Model(fit_CanICA, imgs_4D, params_CanICA, tag=tag, load=load).components_img_
+    # DictLearning_imgs = fit_Model(fit_DictLearning, maps.to_img(), params_DictLearning, tag=tag, load=load).components_img_
+    Ward_imgs = fit_Model(fit_Wards, imgs_4D, params_Wards, tag=tag, load=load).labels_img_
+    Kmeans_imgs = fit_Model(fit_Kmeans, imgs_4D, params_Kmeans, tag=tag, load=load).labels_img_
+
+    atlas_CanICA = Maps(CanICA_imgs, template=template).to_atlas()
+    # atlas_DictLearning = Maps(DictLearning_imgs, template=template).to_atlas()
+    atlas_Ward = Maps(Ward_imgs, template=template).to_atlas()
+    atlas_Kmeans = Maps(Kmeans_imgs, template=template).to_atlas()
+
+    array_avg = maps_avg.to_array()
+    atlas_mean = Maps(array_avg > 0.0003, template=template).to_atlas()
 
     atlas_dict = {
         'Harvard Oxford 0': atlas_HO_0,
@@ -117,6 +129,7 @@ if __name__ == '__main__':
         # 'Dict Learning {} components'.format(n_components): atlas_DictLearning,
         'Ward {} components'.format(n_components): atlas_Ward,
         'Kmeans {} components'.format(n_components): atlas_Kmeans,
+        'Mean thresholded': atlas_mean,
     }
 
     criteria = [
@@ -126,8 +139,8 @@ if __name__ == '__main__':
     for name, atlas in atlas_dict.items():
         plotting.plot_activity_map(atlas['maps'], title=name)
 
-    benchmarks = benchmark(maps.avg(), atlas_dict, criteria, verbose=True)
-    
+    benchmarks = benchmark(maps_avg, atlas_dict, criteria, verbose=True)
+
     print(benchmarks)
     sns.catplot(x='Criterion', y='Value', hue='Atlas', data=benchmarks, height=6, kind="bar", palette="muted").set(yscale="log")
     plt.title('Atlas benchmark on \'{}\' keyword'.format(keyword))
--- clustering.py
+++ clustering.py
@@ -6,8 +6,10 @@ from tools import get_dump_token, pickle_dump, pickle_load
 
 save_dir = pickle_path+'generated_atlases/'
 
+
 def fit_GroupICA(imgs, params):
     raise NotImplementedError('')
+
 
 def fit_CanICA(imgs, params):
     defaults = {
@@ -24,6 +26,7 @@ def fit_CanICA(imgs, params):
 
     return CanICA(**context).fit(imgs)
 
+
 def fit_DictLearning(imgs, params):
     defaults = {
         'n_components': 10,
@@ -32,11 +35,13 @@ def fit_DictLearning(imgs, params):
         'verbose': 1,
         'random_state': 0,
         'n_epochs': 1,
-        'mask_strategy': 'template'
+        'mask_strategy': 'template',
+        'n_jobs': -2
     }
     context = dict(defaults, **params)
 
     return DictLearning(**context).fit(imgs)
+
 
 def fit_Kmeans(imgs, params):
     defaults = {
@@ -46,25 +51,29 @@ def fit_Kmeans(imgs, params):
         'smoothing_fwhm': 10.,
         'memory': 'nilearn_cache',
         'memory_level': 1,
-        'verbose': 1
+        'verbose': 1,
+        'n_jobs': -2
     }
     context = dict(defaults, **params)
 
     return Parcellations(**context).fit(imgs)
-    
+
+
 def fit_Wards(imgs, params):
     defaults = {
-        'method': 'ward', 
+        'method': 'ward',
         'n_parcels': 10,
         'standardize': False,
         'smoothing_fwhm': 2.,
         'memory': 'nilearn_cache',
         'memory_level': 1,
-        'verbose': 1
+        'verbose': 1,
+        'n_jobs': -2
     }
     context = dict(defaults, **params)
-    
+
     return Parcellations(**context).fit(imgs)
+
 
 def fit_Model(Model, imgs, params=dict(), load=True, save=True, tag=None):
     filepath = save_dir+get_dump_token(Model.__name__+'_', tag=tag)
@@ -75,6 +84,3 @@ def fit_Model(Model, imgs, params=dict(), load=True, save=True, tag=None):
 
     model = Model(imgs, params)
     return pickle_dump(model.fit(imgs), filepath, save=save)
-
-if __name__ == '__main__':
-    pass
